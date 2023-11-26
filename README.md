0f1fcf72f2f0df3bd21e0002b9abc80efc4cf543
Tero Saarni <tero.saarni@gmail.com>
2023-11-26 12:45:37 +0200

    add support for melcloud

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Supported sources:
 - [Skoda Connect](https://www.skoda-connect.com/) for car odometer, battery charge percentage and esimated range data.
 - [spot-hinta.fi](https://spot-hinta.fi/) for electricity price.
 - Zigbee and Z-Wave sensors via MQTT broker for temperature and humidity data, among others, using [Z-Wave JS UI](https://github.com/zwave-js/zwave-js-ui) and [Zigbee2MQTT](https://github.com/Koenkk/zigbee2mqtt).
+- [Melcloud](https://www.melcloud.com/) for heat pump data.
 
 ## Development
 
@@ -37,6 +38,12 @@ Run the test web server to see the metrics being pushed to it in Prometheus expo
 python3 tests/httpserver.py
 ```
 
+Build the container image:
+
+```bash
+docker build -t quay.io/tsaarni/home-metrics:latest .
+```
+
 The documentation for used APIs is available at following links:
 
 - [Shelly 1st gen REST API](https://shelly-api-docs.shelly.cloud/gen1/#shelly-family-overview) for Shelly 3EM [status](https://shelly-api-docs.shelly.cloud/gen1/#shelly-3em-status).
@@ -46,3 +53,4 @@ The documentation for used APIs is available at following links:
 - [spot-hinta.fi REST API](https://spot-hinta.fi/) for electricity price.
 - VictoriaMetrics [API for importing](https://docs.victoriametrics.com/url-examples.html#apiv1importprometheus) time series data in Prometheus exposition format.
 - Specification of [Prometheus datatypes](https://github.com/prometheus/docs/blob/main/content/docs/concepts/metric_types.md) and [Prometheus exposition format](https://github.com/prometheus/docs/blob/main/content/docs/instrumenting/exposition_formats.md).
+- [Z-Wave JS UI MQTT](https://zwave-js.github.io/zwave-js-ui/#/guide/mqtt) and Z-Wave [command classes](https://z-wave.me/manual/z-way/Command_Class_Reference.html#).
---
+++ app/melcloud.py
@@ -0,0 +1,96 @@
+import asyncio
+import logging
+
+import datetime
+import httpx
+import prometheus
+import task
+
+MELCLOUD_URI = "https://app.melcloud.com/Mitsubishi.Wifi.Client"
+APP_VERSION = "1.30.5.0"
+
+logger = logging.getLogger("app.melcloud")
+
+
+class Melcloud(object):
+    def configure(self, instance_name, config):
+        self.instance_name = instance_name if instance_name else "melcloud"
+        self.username = config["username"]
+        self.password = config["password"]
+        self.poll_period_sec = config.get("poll-period-sec", 60 * 60)
+        self.database_url = config["database_url"]
+
+    async def start(self):
+        logger.info(f"Starting Melcloud instance_name={self.instance_name} poll_period_sec={self.poll_period_sec}")
+
+        while True:
+            try:
+                await self.update_metrics()
+            except Exception as e:
+                logger.exception("Error:", exc_info=e)
+
+            logger.info(f"Sleeping for {self.poll_period_sec} seconds")
+            await asyncio.sleep(self.poll_period_sec)
+
+    async def update_metrics(self):
+        logger.debug(f"loggin in: username={self.username} password={'<redacted>' if self.password else 'None'}")
+
+        client = httpx.AsyncClient()
+        response = await client.post(
+            f"{MELCLOUD_URI}/Login/ClientLogin",
+            json={
+                "AppVersion": APP_VERSION,
+                "Email": self.username,
+                "Password": self.password,
+            },
+        )
+        if response.status_code != 200:
+            logger.error(f"failed to login: {response.status_code}")
+            raise task.TaskException(f"failed to login: {response.status_code}")
+
+        res = response.json()
+        if res["ErrorId"]:
+            logger.error(f"failed to login ErrorId={res['ErrorId']} ErrorMessagre={res['ErrorMessage']}")
+            raise task.TaskException(f"failed to login ErrorId={res['ErrorId']} ErrorMessagre={res['ErrorMessage']}")
+
+        logger.debug(f"getting devices")
+        response = await client.get(
+            f"{MELCLOUD_URI}/User/ListDevices",
+            headers={
+                "X-Mitscontextkey": res["LoginData"]["ContextKey"],
+            },
+        )
+
+        if response.status_code != 200:
+            logger.error(f"failed to get devices: {response.status_code}")
+            raise task.TaskException(f"failed to get devices: {response.status_code}")
+
+        res = response.json()
+        dev = res[0]["Structure"]["Devices"][0]["Device"]
+
+        last_report = datetime.datetime.fromisoformat(dev["LastTimeStamp"])
+        timestamp = last_report.timestamp() * 1000
+
+        metrics = prometheus.Metrics()
+
+        metrics.gauge(
+            "temperature_celsius",
+            "Temperature in Celsius",
+            labels={"sensor": self.instance_name},
+        ).add(dev["RoomTemperature"], timestamp_msec=timestamp)
+
+        metrics.gauge(
+            "target_temperature_celsius",
+            "Target temperature in Celsius",
+            labels={"sensor": self.instance_name},
+        ).add(dev["SetTemperature"], timestamp_msec=timestamp)
+
+        metrics.gauge(
+            "electric_consumption_kwh", "Energy consumption in kWh", labels={"sensor": self.instance_name}
+        ).add(dev["CurrentEnergyConsumed"] / 1000, timestamp_msec=timestamp)
+
+        logger.info(f"Storing metrics: url={self.database_url}")
+        await client.post(self.database_url, content=metrics.format())
+
+
+task.register(Melcloud, "melcloud")
